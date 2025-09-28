f8f3dd45be79f111be3f460cc3264939ac25f34c
K Srinivas Rao <ksrinivasrao531@gmail.com>
2025-09-28 11:11:07 +0530

    update

--- db/config.ts
+++ db/config.ts
@@ -6,10 +6,6 @@ const connectionString = process.env.DATABASE_URL || "";
 
 const pool = new Pool({
   connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
